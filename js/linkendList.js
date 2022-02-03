ae6f183641ab606f8729d5f61289bd0347c325a8
EdgarMTDev <edgarmontiel961@gmail.com>
2022-02-03 13:28:57 -0600

    domContent

--- js/linkendList.js
+++ js/linkendList.js
@@ -1,52 +1,57 @@
 "use strict";
 
-const buttonAdd = document.getElementById("button_agregar");
-const buttonDelete = document.getElementById("button_delete");
-const buttonModif = document.getElementById("button_modif");
+document.addEventListener('DOMContentLoaded', () => {
 
-const parentList = document.querySelector(".container_list");
 
-buttonAdd.addEventListener("click", () => {
+    const buttonAdd = document.getElementById("button_agregar");
+    const buttonDelete = document.getElementById("button_delete");
+    const buttonModif = document.getElementById("button_modif");
 
-});
+    const parentList = document.querySelector(".container_list");
 
-// Delete nodes
-buttonDelete.addEventListener("click", () => {
-    const deleteIndex = document.getElementById("delete--index").value;
-    animationDeleteNode(deleteIndex);
-    setTimeout(() => {
-        deleteNode(deleteIndex);
-    }, 2000);
-});
+    buttonAdd.addEventListener("click", () => {
 
-// Proximo Sprint: add node in the list
-function createNode(arg) {
-    // Pendient Process
-}
+    });
 
-function animationAddNode(arg) {
+    // Delete nodes
+    buttonDelete.addEventListener("click", () => {
+        const deleteIndex = document.getElementById("delete--index").value;
+        animationDeleteNode(deleteIndex);
+        setTimeout(() => {
+            deleteNode(deleteIndex);
+        }, 2000);
+    });
 
-}
-
-async function deleteNode(arg) {
-    let childs = parentList.getElementsByTagName("div");
-
-    if (arg % 2 === 0) {
-        childs[arg].remove();
-        childs[arg++].remove();
-    } else {
-        alert("EL nodo no coincide");
+    // Proximo Sprint: add node in the list
+    function createNode(arg) {
+        // Pendient Process
     }
-}
 
-function animationDeleteNode(arg) {
-    let childs = parentList.getElementsByTagName("div");
+    function animationAddNode(arg) {
 
-    if (arg % 2 === 0) {
-        childs[arg].style.animation = `deletePointer 2s`;
-        const arrow = document.getElementById(`arrow_${Number(arg) + 1}`);
-        arrow.style.animation = `deletePointer 2s`;
-    } else {
-        alert("EL nodo no coincide");
     }
-}
+
+    async function deleteNode(arg) {
+        let childs = parentList.getElementsByTagName("div");
+
+        if (arg % 2 === 0) {
+            childs[arg].remove();
+            childs[arg++].remove();
+        } else {
+            alert("EL nodo no coincide");
+        }
+    }
+
+    function animationDeleteNode(arg) {
+        let childs = parentList.getElementsByTagName("div");
+
+        if (arg % 2 === 0) {
+            childs[arg].style.animation = `deletePointer 2s`;
+            const arrow = document.getElementById(`arrow_${Number(arg) + 1}`);
+            arrow.style.animation = `deletePointer 2s`;
+        } else {
+            alert("EL nodo no coincide");
+        }
+    }
+
+})
--- js/theme.js
+++ js/theme.js
@@ -1,28 +1,29 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", () => {
-    let iconTheme = document.getElementById("icon_theme");
-    let ligthcolor = "rgb(255, 255, 255)";
 
-    iconTheme.addEventListener("click", () => {
-        let background = document.getElementById("background");
-        let iconMenu = document.getElementById("icon_menu");
-        let navbar = document.querySelector(".navbar");
-        let titleInputs = document.querySelector(".title_inputs");
-        let arrowLink = document.querySelector(".bxs-arrow-from-left");
+    document.addEventListener("DOMContentLoaded", () => {
+        let iconTheme = document.getElementById("icon_theme");
+        let ligthcolor = "rgb(255, 255, 255)";
 
-        if (iconTheme.style.color == ligthcolor) {
-            iconTheme.style.color = "#1a1a1a";
-            background.style.backgroundColor = "#FFFFFF";
-            iconMenu.style.color = "#000000";
-            navbar.style.color = "#000000";
-            titleInputs.style.color = "#000000";
-        } else {
-            iconTheme.style.color = "#FFFFFF";
-            background.style.backgroundColor = "#1a1a1a";
-            iconMenu.style.color = "#FFFFFF";
-            navbar.style.color = "#FFFFFF";
-            titleInputs.style.color = "#FFFFFF";
-            // arrowLink.style.color = "#FFFFFF"
-        }
+        iconTheme.addEventListener("click", () => {
+            let background = document.getElementById("background");
+            let iconMenu = document.getElementById("icon_menu");
+            let navbar = document.querySelector(".navbar");
+            let titleInputs = document.querySelector(".title_inputs");
+            let arrowLink = document.querySelector(".bxs-arrow-from-left");
+
+            if (iconTheme.style.color == ligthcolor) {
+                iconTheme.style.color = "#1a1a1a";
+                background.style.backgroundColor = "#FFFFFF";
+                iconMenu.style.color = "#000000";
+                navbar.style.color = "#000000";
+                titleInputs.style.color = "#000000";
+            } else {
+                iconTheme.style.color = "#FFFFFF";
+                background.style.backgroundColor = "#1a1a1a";
+                iconMenu.style.color = "#FFFFFF";
+                navbar.style.color = "#FFFFFF";
+                titleInputs.style.color = "#FFFFFF";
+                // arrowLink.style.color = "#FFFFFF"
+            }
+        });
     });
-});
